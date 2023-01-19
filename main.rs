f65692f3910c37c6b586dafaadd4bd5e1cc5a7ce
Egg <85519326+Egg55@users.noreply.github.com>
2023-01-19 12:02:40 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,7 +1,39 @@
-use std::env;
-use std::process;
-fn main() {
-    let args: Vec<String> = env::args().collect();
+use std::{io::{stdout, Write,Stdout}, thread, time};
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    terminal, cursor, style::{self, Stylize, Color}, Result
+};
+
+struct Point {
+    x: u16,
+    y: u16,
+}
+
+fn main() -> Result<()> {
+    let CSL_SIZE_X = 50;
+    let CSL_SIZE_Y = 25;
+
+    let mut ball = Point{x:10, y:10};
+    let mut old_ball_pos = Point{x:10, y:10};
+
+  
+    let mut stdout = stdout();
     
-    println!("{:?}",args)
-    }
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    stdout.execute(cursor::Hide)?;
+    
+    draw_base(stdout,CSL_SIZE_Y,1);
+  
+    Ok(())
+}
+
+fn draw_base(mut stdout: Stdout,size_y: u16, base_type: u8) -> Result<()> {
+  stdout
+        .queue(cursor::MoveTo(0,size_y-4))?
+        .queue(style::PrintStyledContent(":".with(Color::Rgb { r: (50), g: (50), b: (50) })))?
+        .queue(style::PrintStyledContent("___________".with(Color::Rgb { r: (100), g: (200), b: (0) })))?
+        .queue(style::PrintStyledContent("./~~~\\.".with(Color::Rgb { r: (100), g: (0), b: (0) })))?
+        .queue(style::PrintStyledContent("___________".with(Color::Rgb { r: (100), g: (200), b: (0) })))?
+        .queue(style::PrintStyledContent("\n \\                           / \n  \\_________________________/ \n  (_)                     (_)".with(Color::Rgb { r: (50), g: (50), b: (50) })))?;
+  Ok(())
+}
